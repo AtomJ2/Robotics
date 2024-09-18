85e46f1340f343b556f6610d52a8ec468b839cec
Yaroslav Chekanov <42699170+AtomJ2@users.noreply.github.com>
2024-09-18 09:43:35 +0700

    Delete lab2/ex03 directory

--- lab2/ex03/ros2_ws3/build/atom_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/atom/ros2_ws3/install/atom_package'
--- lab2/ex03/ros2_ws3/src/atom_package/atom_package/my_node.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from atom_package.')
-
-
-if __name__ == '__main__':
-    main()
