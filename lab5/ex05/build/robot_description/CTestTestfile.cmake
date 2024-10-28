a13f84e746064bdbcc17d57617bd3f6c8c65b450
Atom <atom.j2@mail.ru>
2024-10-28 14:37:04 +0700

    lab5

---
+++ lab5/ex05/build/robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/atom/Robotics/lab5/ex04/robot_description
+# Build directory: /home/atom/Robotics/lab5/ex04/build/robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
