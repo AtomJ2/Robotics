4f40aa38c92cbc4d55c73052784b45693066f5ce
Yaroslav Chekanov <42699170+AtomJ2@users.noreply.github.com>
2024-11-04 21:18:10 +0700

    Delete lab5/ex05 directory

--- lab5/ex05/build/robot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atom/Robotics/lab5/ex04/robot_bringup
-# Build directory: /home/atom/Robotics/lab5/ex04/build/robot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lab5/ex05/build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atom/Robotics/lab5/ex04/robot_description
-# Build directory: /home/atom/Robotics/lab5/ex04/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
